f7d2b822eac4279f0ffd36f9469ad401fd450fc2
shivamtiwari5121 <76442299+shivamtiwari5121@users.noreply.github.com>
2021-06-05 13:42:57 -0400

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascipt and react.js</h1>
+      <p>
+        This was an amazing bootcamp taken up by Shaurya Sinha and learned by
+        Shivam Tiwari We covered everything from Scarch including Javascript
+        Reactjs, HTML.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
